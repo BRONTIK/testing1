655bfecaa0fc3ae63daa14f340d7ce0e044b70dd
BRONTIK <50723771+BRONTIK@users.noreply.github.com>
2019-05-17 12:39:42 +0300

    Update main.c

--- app/main.c
+++ app/main.c
@@ -4,8 +4,11 @@
 
 int main()
 {
-    int a, b, c;
-    double x1 = 0, x2 = 0;
+    int a;
+    int b;
+    int c;
+    double x1 = 0;
+    double x2 = 0;
      if (scanf("%d %d %d", &a, &b, &c) == 3) {
         printf("%d %d %d", a, b, c);
           search(a, b, c, &x1, &x2);
